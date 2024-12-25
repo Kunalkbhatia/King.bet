f13d5a02faef6cb181d204b2cfe2fc503a19ef54
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-12-25 16:35:34 +0530

    first commit

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,8 @@
+export default function SideBar() {
+  return (
+  <div className="w-[150px] h-screen bg-black bg-opacity-20 p-2">
+    <div className="text-3xl font-bold">King<span className="text-xl">.bet</span></div>
+    <div></div>
+  </div>
+  );
+}
