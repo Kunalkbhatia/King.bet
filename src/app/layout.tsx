d7991d102c217aaf01f8b459f323d1c3d4996e60
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-12-25 16:41:02 +0530

    Added lilita font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,12 @@ export const lilitaOne = Lilita_One({
   variable: "--font-lilita-one",
   subsets: ["latin"],
   weight: "400",
-})
+});
 
 export const metadata: Metadata = {
   title: "King.bet",
-  description: "King.bet: Play competitive chess and place bets to win big in a thrilling strategic showdown!",
+  description:
+    "King.bet: Play competitive chess and place bets to win big in a thrilling strategic showdown!",
 };
 
 export default function RootLayout({
@@ -32,13 +33,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+  className={`${geistSans.variable} ${geistMono.variable} ${lilitaOne.variable} antialiased`}
+>
         <div className="flex bg-[url(https://assets-themes.chess.com/image/drtgi/background.jpg)] bg-cover bg-center bg-fixed">
-        <SideBar/>
-        <div className="flex-1">
-        {children}
-        </div>
+          <SideBar />
+          <div className="flex-1">{children}</div>
         </div>
       </body>
     </html>
