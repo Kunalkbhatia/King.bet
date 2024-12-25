fbe48c0aae64cc10c86c25003daa801d48e443c1
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-12-25 16:48:26 +0530

    added logo

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image"
+import kingLogo from "../../../public/chess-svgrepo-com.svg";
 export default function SideBar() {
   return (
-  <div className="w-[150px] h-screen bg-black bg-opacity-20 p-2">
-    <div className="text-4xl font-bold font-lilita">King<span className="text-xl">.bet</span></div>
+  <div className="w-[180px] h-screen bg-black bg-opacity-20">
+    <div className="flex place-items-center">
+    <Image src={kingLogo} alt="logo" width={40} height={40}/>
+    <div className="text-5xl font-bold font-lilita">King<span className="text-xl">.bet</span></div>
+    </div>
     <div></div>
   </div>
   );
